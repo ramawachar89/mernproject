05083d81ae30da1d7a1acb2c9bba7852203501bf
ramawachar <ramawachar621@gmail.com>
2023-05-15 00:13:50 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './screens/Home';
 import Login from './screens/Login';
 import Signup from './screens/Signup';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
